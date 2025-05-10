307a485f0fe8cb4f9a33a1c0a9326a829856bcb8
rais17 <nehalarshad11@gmail.com>
2025-05-10 23:46:55 +0530

    Changes In Feed Page

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -7,7 +7,7 @@ const Feed = () => {
   const { data, isLoading, isError, error } = useGetUserFeedQuery();
   
   if (isLoading) return <div>Loading...</div>
-  if (isError) return <div>Error...</div>
+  if (isError) return <div>Error is in Feed Page...</div>
 
   return (
     <div className='flex justify-center items-center h-[calc(100vh-64px)]'>
